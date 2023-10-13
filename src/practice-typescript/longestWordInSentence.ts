3ebf19828241a5003f66846526b5c869b2e6eaea
Jo Kalnavarna <madara.k@hotmail.co.uk>
2023-10-13 17:09:49 +0100

    corrected function

--- src/practice-typescript/longestWordInSentence.ts
+++ src/practice-typescript/longestWordInSentence.ts
@@ -1,18 +1,15 @@
 export default function longestWord(sentence: string): string {
   //need to split the word as we can't directly use .length on string
-  const splitArray = sentence.split('');
+  const splitArray = sentence.split(' ');
   //initialize a variable to store the longest word
   let longestWord = '';
-
-  //for loop to identify which word in the index is the longest in length
-  for (let i = 0; i < splitArray.length; i++) {
-    if (splitArray[i].length > longestWord.length) {
-      longestWord = splitArray[i];
+  //for loop to identify which word in the index is the longest in length and then
+  // assign that to a let by checking its index
+  for (let index = 0; index < splitArray.length; index++) {
+    if (splitArray[index].length > longestWord.length) {
+      longestWord = splitArray[index];
     }
   }
   return longestWord;
 }
-
-//assign that to a let by checking its index
-
 console.log(longestWord('hello world'));
