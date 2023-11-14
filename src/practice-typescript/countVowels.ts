455d1e088366f117885b75ec822bbe56ab5dedb9
Jo Kalnavarna <madara.k@hotmail.co.uk>
2023-11-14 12:24:20 +0000

    more robust approach

--- src/practice-typescript/countVowels.ts
+++ src/practice-typescript/countVowels.ts
@@ -1,19 +1,14 @@
-const VOWELS = ['a', 'e', 'i', 'o', 'u'];
+const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
 
 function countVowels(text: string): number {
-  //need to get all strings to lower case
-  const lowerCaseString = text.toLowerCase();
-  // need count variable to count from 0
+  //count const
   let count = 0;
-  // for loop to loop through to see the lenght
-  console.log('whats my length', lowerCaseString.length);
-  for (let index = 0; index < lowerCaseString.length; index++) {
-    // if vow is in it, count++
-    if (VOWELS.includes(lowerCaseString[index])) {
+  // for of loop
+  for (const char of text.toLowerCase()) {
+    if (vowels.has(char)) {
       count++;
     }
   }
-  // return the count
   return count;
 }
 
