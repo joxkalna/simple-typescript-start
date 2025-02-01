d0ac0c4686e28d6d96e70ccb2755c8efb45d1d15
Jo Kalnavarna <madara.k@hotmail.co.uk>
2025-02-01 16:24:13 +0000

    use forEach for oddsAndEven

--- src/Katas/oddAndEven.js
+++ src/Katas/oddAndEven.js
@@ -1,19 +1,29 @@
-function oddEven(numbers) {
-    const odd = []
-    const even = []
+/* 
+map in this case is not a good example, because map is used to transform each element of an array
+and return a new array with the results. Its always returns an array of the same length as the input
 
-    numbers.map((number) => number % 2 === 0 ? even.push(number) : odd.push(number))
-    return { odd, even }
-}
+*/
+// const oddsAndEvens = (inputArray) => {
+//   const odds = [];
+//   const even = [];
+//   inputArray.map((number) =>
+//     number % 2 === 0 ? even.push(number) : odds.push(number),
+//   );
+//   return { odds, even };
+// };
+/*
+forEach is used when you want to perform an operation on each element of an array
+but don't need to return a new array
+*/
 
-console.log(oddEven([1, 2, 3, 5, 7, 8]))
-
-const oddsAndEvens = number => {
-    const odd = []
-    const even = []
-
-    number.map((number) => number % 2 === 0 ? even.push(number) : odd.push(number))
-    return { odd, even }
-}
-
-console.table(oddsAndEvens([1, 2, 3, 5, 7, 8]))
+const oddsAndEvenWithLoop = (inputArray) => {
+  const odds = [];
+  const even = [];
+  inputArray.forEach((currentNumber) =>
+    currentNumber % 2 === 0
+      ? even.push(currentNumber)
+      : odds.push(currentNumber),
+  );
+  return { odds, even };
+};
+console.log(oddsAndEvenWithLoop([1, 2, 3, 4, 5, 6, 7]));
